c21d8681aeb2d9b6f8d25156ca6b6733a0cd4b97
UserKazun <kazuusjlo8@gmail.com>
2021-10-05 00:02:08 +0900

    Implement fh subcommand.

--- cli/cli.go
+++ cli/cli.go
@@ -1,6 +1,11 @@
 package cli
 
-import "io"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 const (
 	ExitCodeOK             = 0
@@ -17,5 +22,22 @@ func NewCli(outStream, errStream io.Writer) *CLI {
 }
 
 func (c *CLI) Run(args []string) int {
+	//var filename string
+
+	flags := flag.NewFlagSet("fh", flag.ExitOnError)
+	flags.SetOutput(c.errStream)
+
+	filename := flags.String("filename", "", "Allowed extensions: .csv")
+
+	fmt.Println(os.Args[1])
+	if len(os.Args) < 2 {
+		fmt.Println("The file name must be specified.")
+		os.Exit(1)
+	} else {
+		flags.Parse(os.Args[2:])
+		fmt.Println("filename:", *filename)
+		os.Exit(1)
+	}
+
 	return ExitCodeOK
 }
